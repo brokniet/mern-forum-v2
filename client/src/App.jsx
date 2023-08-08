5d79b2e396874610caed45ef90e2311276fad422
brokniet <kurhopwf13@gmail.com>
2023-08-07 22:38:20 -0300

    Adding get endpoint and fetching that into the UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch('http://localhost:5000/posts');
+      const newPosts = await response.json();
+      setPosts(newPosts);
+    }
+    fetchPosts();
+  }, []);
+
   return (
     <div className="app">
 
       <header className="header">
         <div className="title-container">
           <h1 className="title">Welcome to my Forum!</h1>
-          <h5 className="subtitle">Feel free to leave your feedback or anything you like.</h5>
+          <h5 className="subtitle">Feel free to leave your feedback or anything you like</h5>
         </div>
         <button className="add-entry">Add entry</button>
       </header>
 
       <main className="main">
-        
+        <ul className="posts">
+          {
+            posts.map(post => {
+              return(
+                <li key={post._id}>{post.title}</li>
+              )
+            })
+          }
+        </ul>
       </main>
 
     </div>
--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import 'dotenv/config';
 import Post from './models/Post.js';
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+
+app.get('/posts', async (req, res) => {
+    const posts = await Post.find();
+    res.json(posts);
+});
 
 app.post('/posts', async (req, res) => {
     const { title, content } = req.body;
@@ -19,6 +27,6 @@ app.post('/posts', async (req, res) => {
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log(`listening on port ${process.env.PORT}`);
     app.listen(process.env.PORT);
-})
+});
 
 
