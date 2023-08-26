598691c2a87efe2a6dd985db162e060bbe6f549d
brokniet <kurhopwf13@gmail.com>
2023-08-25 23:01:29 -0300

    Add loader UI and logic

--- client/src/components/PostsList.jsx
+++ client/src/components/PostsList.jsx
@@ -8,11 +8,13 @@ import "../styles/App.css";
 export default function PostsList() {
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchPosts() {
       const newPosts = await getPosts();
       setPosts(newPosts.reverse());
+      setIsLoading(false);
     }
     fetchPosts();
   }, []);
@@ -24,58 +26,67 @@ export default function PostsList() {
 
   return (
     <>
-      <form className="filters-container" onSubmit={(e) => e.preventDefault()}>
-        <input
-          type="text"
-          id="title-filter"
-          placeholder="Search..."
-          onChange={(e) => setSearch(e.target.value)}
-          value={search}
-        />
-      </form>
-      <ul className="posts">
-        {posts
-          .filter((post) => {
-            return search.toLowerCase() === ""
-              ? post
-              : post.title.toLowerCase().includes(search);
-          })
-          .map((post) => {
-            return (
-              <li key={post._id}>
-                <Link to={`posts/${post._id}`}>{post.title}</Link>
-                <div className="buttons-container">
-                  <Link to={`posts/editPost/${post._id}`}>
-                    <button className="edit-button">✏️</button>
-                  </Link>
-                  <Popup trigger={<button>❌</button>} modal>
-                    {(close) => (
-                      <div className="delete-modal">
-                        <span className="delete-modal-title">
-                          Are you sure you want to delete the post?
-                        </span>
-                        <div className="delete-modal-button-container">
-                          <button
-                            className="delete-modal-button"
-                            onClick={() => handleDeletePost(post._id)}
-                          >
-                            Yes
-                          </button>
-                          <button
-                            className="delete-modal-button"
-                            onClick={() => close()}
-                          >
-                            No
-                          </button>
-                        </div>
-                      </div>
-                    )}
-                  </Popup>
-                </div>
-              </li>
-            );
-          })}
-      </ul>
+      {isLoading ? (
+        <div class="custom-loader"></div>
+      ) : (
+        <>
+          <form
+            className="filters-container"
+            onSubmit={(e) => e.preventDefault()}
+          >
+            <input
+              type="text"
+              id="title-filter"
+              placeholder="Search..."
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+            />
+          </form>
+          <ul className="posts">
+            {posts
+              .filter((post) => {
+                return search.toLowerCase() === ""
+                  ? post
+                  : post.title.toLowerCase().includes(search);
+              })
+              .map((post) => {
+                return (
+                  <li key={post._id}>
+                    <Link to={`posts/${post._id}`}>{post.title}</Link>
+                    <div className="buttons-container">
+                      <Link to={`posts/editPost/${post._id}`}>
+                        <button className="edit-button">✏️</button>
+                      </Link>
+                      <Popup trigger={<button>❌</button>} modal>
+                        {(close) => (
+                          <div className="delete-modal">
+                            <span className="delete-modal-title">
+                              Are you sure you want to delete this post?
+                            </span>
+                            <div className="delete-modal-button-container">
+                              <button
+                                className="delete-modal-button"
+                                onClick={() => handleDeletePost(post._id)}
+                              >
+                                Yes
+                              </button>
+                              <button
+                                className="delete-modal-button"
+                                onClick={() => close()}
+                              >
+                                No
+                              </button>
+                            </div>
+                          </div>
+                        )}
+                      </Popup>
+                    </div>
+                  </li>
+                );
+              })}
+          </ul>
+        </>
+      )}
     </>
   );
 }
