fcf40fab12b73eb3e711c963bcb902a92bb08533
brokniet <kurhopwf13@gmail.com>
2023-09-25 22:46:47 -0300

    Add rate limit for the API

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import "dotenv/config";
 import { getPostsController } from "./controllers/getPostsController.js";
 import { deletePostController } from "./controllers/deletePostController.js";
@@ -10,10 +11,16 @@ import { getPostController } from "./controllers/getPostController.js";
 import { editPostController } from "./controllers/editPostController.js";
 
 const app = express();
+const rateLimitMiddleware = rateLimit({
+  max: 10,
+  windowMs: 60 * 1000,
+  message: "You have exceeded your 10 requests per minute limit.",
+});
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
+app.use(rateLimitMiddleware);
 
 app.get("/posts", getPostsController);
 app.post("/posts", createPostController);
